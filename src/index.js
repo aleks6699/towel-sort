9c0804cdb0c1fea52e7eefe300b73da8dbcd93c9
Oleksandr <aleks6699@gmail.com>
2024-09-22 19:28:27 +0300

    feat: add task tower-sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (!matrix || matrix.length === 0) {
+        return [];
+    }
+
+    return matrix.reduce((acc, currentRow, index) => {
+        if (index % 2 === 0) {
+            return acc.concat(currentRow);
+        } else {
+            return acc.concat(currentRow.reverse());
+        }
+    }, []);
+};
